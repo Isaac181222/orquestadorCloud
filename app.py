0e44f62eef40b0ed6c1c721a564797a5863eb7b7
Isaac_Ss <142519991+Isaac181222@users.noreply.github.com>
2025-05-10 23:44:41 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+import requests
+
+app = FastAPI()
+
+@app.get("/usuario/{id}/historial")    #CAMBIAR LOS PUERTOS
+def historial_usuario(id: int):
+    usuario = requests.get(f"http://microservicio-usuarios:5000/usuarios/{id}").json()
+    prestamos = requests.get(f"http://microservicio-prestamos:3000/prestamos/usuario/{id}").json()
+
+    historial = []
+    for p in prestamos:
+        libro = requests.get(f"http://microservicio-libros:8080/libros/{p['id_libro']}").json()
+        historial.append({
+            "libro": libro["titulo"],
+            "fecha_prestamo": p["fecha_prestamo"],
+            "estado": p["estado"]
+        })
+
+    return {
+        "usuario": usuario,
+        "prestamos": historial
+    }
